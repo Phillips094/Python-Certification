cb20a34cee9eed1ac2c27e9921be4aa1f6de2229
Felipe Gomez <fdjesusgomez94@berkeley.edu>
2024-09-01 21:36:04 -0700

    Completed Section 10; Starting Section 11: Milestone 2

---
+++ cap.py
@@ -0,0 +1,7 @@
+def cap_text(text):
+	'''
+	Input a string. '.capitalize()' upper cases the first letter. '.title()' upper cases
+	first letter of every word.
+	Output the capitalized string.
+	'''
+	return text.title() #changed from .capitalize() which only capitalizes the first word only of a string #title() capitalizes every single word of a string
---
+++ simple1.py
@@ -0,0 +1,14 @@
+'''
+A Very Simple Script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1 #to avoid using single letter variables
+    second = 2 #to avoid using single letter variables
+    print(first)
+    print(second)
+
+myfunc()
